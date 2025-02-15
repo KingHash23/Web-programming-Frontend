1897dabf759021467b22eaedb906dd009a296aaa
KingHash23 <markcalvin2003@gmail.com>
2025-02-15 18:16:12 +0300

    variables and constants

---
+++ Hellohtml/frontend/Week5/HelloWorld.js
@@ -0,0 +1,50 @@
+console.log("Hello World");
+let num =5;
+console.log(num);
+console.log(typeof(num));
+
+//  We use var or let kewords to declare variables
+var tea;
+let name;
+
+//  We use const keyword to declare variables that cannot be reassigned
+const PI = 3.14;
+console.log(PI);
+
+//declare variable num
+let num1;
+//  Assign a value to num
+num1 = 5;
+//declare variable num2 and assign 5 to it
+let num2 = 5;
+let num3=6;
+let num4=7, num5=8, num6=9, num7=10;
+console.log(num, num1, num2, num3, num4, num5, num6, num7);
+//assign 5 to a variable score;
+let score = 5;
+console.log(score);
+//change score to 3
+score = 3;
+console.log(score);
+// rules of naming js variables
+//valid
+let messages = "hello";
+let _messages = "hello";
+let $messages = "hello";
+console.log(messages, _messages, $messages);
+// variable name are case sensitive eg;
+let age = 78;
+let Age = 23;
+console.log(age);
+console.log(Age);
+
+//js constants
+// assign 5 to num
+const number = 5;
+// number = 10
+console.log(number);
+
+// const x;
+//  x = 5;
+//  console.log(x);
+ 
